78929280a2b767b576fa33335aabc95915d1f31f
Francesco Calì <francescocali.97@gmail.com>
2020-04-24 12:37:51 +0200

    Prove animazioni da input

---
+++ GDG_game/Assets/Animazioni/MoveCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    Animator anim;
+    float s;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        if (move != 0)
+        {
+            s += 0.01f;
+        }
+        else
+        {
+            if (s > 0) s -= 0.05f;
+            else s = 0;
+        }
+        anim.SetFloat("Speed", s);
+
+    }
+}
