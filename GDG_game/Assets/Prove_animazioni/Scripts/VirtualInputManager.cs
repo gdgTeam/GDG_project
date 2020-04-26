5aa1f92cfaf54c76554f85e8a48af42ebcbb5367
Francesco Calì <francescocali.97@gmail.com>
2020-04-26 18:19:53 +0200

    Movimento sistemato con singleton

---
+++ GDG_game/Assets/Prove_animazioni/Scripts/VirtualInputManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    public class VirtualInputManager : Singleton<VirtualInputManager>
+    {
+        public bool MoveRight;
+        public bool MoveLeft;
+        public bool Running;
+    }
+}
