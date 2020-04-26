cf45a17d492e261efb2135462f9e940b817df79f
Francesco Calì <francescocali.97@gmail.com>
2020-04-26 18:47:12 +0200

    Implementazione state behaviour base

--- GDG_game/Assets/Prove_animazioni/Scripts/CharacterControl.cs
+++ GDG_game/Assets/Prove_animazioni/Scripts/CharacterControl.cs
@@ -13,34 +13,6 @@ namespace roundbeargames_tutorial
     {
         public float Speed;
         public Animator animator;
-
-        void Update()
-        {
-            if (VirtualInputManager.Instance.MoveRight && VirtualInputManager.Instance.MoveLeft)
-            {
-                animator.SetBool(TransitionParameter.Move.ToString(), false);
-                return;
-            }
-
-            if (!VirtualInputManager.Instance.MoveRight && !VirtualInputManager.Instance.MoveLeft)
-            {
-                animator.SetBool(TransitionParameter.Move.ToString(), false);
-            }
-
-            if (VirtualInputManager.Instance.MoveRight)
-            {
-                this.gameObject.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-                this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                animator.SetBool(TransitionParameter.Move.ToString(), true);
-            }
-
-            if (VirtualInputManager.Instance.MoveLeft)
-            {
-                this.gameObject.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-                this.gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-                animator.SetBool(TransitionParameter.Move.ToString(), true);
-            }
-        }
     }
 }
 
