9af1f17a6b65996b9589d02b643b0268970e8a40
Francesco Calì <francescocali.97@gmail.com>
2020-05-02 19:54:31 +0200

    Implementazione caduta libera (bug nel'atterraggio)

--- GDG_game/Assets/Prove_animazioni/Scripts/StateBase/MoveForward.cs
+++ GDG_game/Assets/Prove_animazioni/Scripts/StateBase/MoveForward.cs
@@ -40,19 +40,19 @@ namespace roundbeargames_tutorial
             //controllo che non ci sia niente davanti il character, se rilevo qualcosa non mi muovo più 
             if (control.MoveRight)
             {
+                control.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                 if (!CheckFront(control))
                 {
                     control.transform.Translate(Vector3.forward * Speed * SpeedGraph.Evaluate(stateInfo.normalizedTime) * Time.deltaTime);
-                    control.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                 }
             }
 
             if (control.MoveLeft)
             {
+                control.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                 if (!CheckFront(control))
                 {
                     control.transform.Translate(Vector3.forward * Speed * SpeedGraph.Evaluate(stateInfo.normalizedTime) * Time.deltaTime);
-                    control.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                 }
             }
         }
