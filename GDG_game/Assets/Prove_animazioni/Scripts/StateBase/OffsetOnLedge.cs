786a23dbae3a3458e651f81e9b9058f50fbdd0f6
Fabiana <fabyyy.spa@gmail.com>
2020-05-11 22:29:25 +0200

    Attacco Edge
    (dovrebbe funzionare, non ho capito se ogni tanto il mio PC impazzisce)

---
+++ GDG_game/Assets/Prove_animazioni/Scripts/StateBase/OffsetOnLedge.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/OffSetOnLedge")]
+    public class OffsetOnLedge : StateData
+    {
+        public Vector3 offsetTestaPiedi;
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+            CharacterControl control = characterState.GetCharacterControl(animator);
+            control.GetComponentInParent<Rigidbody>().transform.localPosition = control.ledgeChecker.grabbedLedge.GetComponent<Rigidbody>().transform.position-offsetTestaPiedi;
+
+        }
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            
+           
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+
+       
+    }
+}
