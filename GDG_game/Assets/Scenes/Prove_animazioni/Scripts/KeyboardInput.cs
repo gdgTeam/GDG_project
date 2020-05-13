c40c94371d046389a5a03b6cb10a74fd00f796f6
Pietro <pingi.pie95@gmail.com>
2020-05-13 18:18:46 +0200

    Cartelle ordinate e prova Sfera (da rifare)

---
+++ GDG_game/Assets/Scenes/Prove_animazioni/Scripts/KeyboardInput.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    public class KeyboardInput : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                VirtualInputManager.Instance.MoveRight = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.MoveRight = false;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                VirtualInputManager.Instance.MoveUp = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.MoveUp = false;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                VirtualInputManager.Instance.MoveDown = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.MoveDown = false;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                VirtualInputManager.Instance.MoveLeft = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.MoveLeft = false;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                VirtualInputManager.Instance.Jump = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.Jump = false;
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                VirtualInputManager.Instance.Pushing = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.Pushing = false;
+            }
+
+            if (Input.GetKey(KeyCode.Q))
+            {
+                VirtualInputManager.Instance.Picking = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.Picking = false;
+            }
+        }
+    }
+}
+
